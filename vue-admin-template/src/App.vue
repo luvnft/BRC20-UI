e02e0810b252abc69517c4af2be61cf50b86fb9d
username <useremail@qq.com>
2024-02-04 13:51:33 +0800

    1

--- vue-admin-template/src/App.vue
+++ vue-admin-template/src/App.vue
@@ -5,21 +5,21 @@
 </template>
 
 <script>
-// export default {
-//   name: 'App'
-// }
-// </script>
-import { ElNotification } from "element-plus"
-import { h } from "vue"
-ElNotification({
-  title: "记住地址不迷路：https://bithome.top",
-  message: h(
-    "a",
-    { style: "color: teal", target: "_blank", href: "https://t.zsxq.com/0fj2rWnlJ" },
-    "小哥哥，小姐姐，加入我的知识星球吧，你的赞赏是我最大的动力"
-  ),
-  duration: 0,
-  position: "bottom-right"
-})
+export default {
+  name: 'App'
+}
+// // npm install element-plus
+// import { ElNotification } from "element-plus"
+// import { h } from "vue"
+// ElNotification({
+//   title: "记住地址不迷路：https://bithome.top",
+//   message: h(
+//     "a",
+//     { style: "color: teal", target: "_blank", href: "https://t.zsxq.com/0fj2rWnlJ" },
+//     "小哥哥，小姐姐，加入我的知识星球吧，你的赞赏是我最大的动力"
+//   ),
+//   duration: 0,
+//   position: "bottom-right"
+// })
 </script>
 
