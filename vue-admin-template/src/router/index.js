21cfe824e924393e73cec132829a7f39919bffb7
username <useremail@qq.com>
2024-02-04 17:42:59 +0800

    1

--- vue-admin-template/src/router/index.js
+++ vue-admin-template/src/router/index.js
@@ -8,12 +8,6 @@ import Layout from '@/layout'
 
 export const constantRoutes = [
   {
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
-  },
-
-  {
     path: '/404',
     component: () => import('@/views/404'),
     hidden: true
