18a6000a3dced936b5c95a6d366bb509073f2f32
username <useremail@qq.com>
2024-02-25 10:03:34 +0800

    1

---
+++ GO/hellow.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("嗨客网(www.haicoder.net)")
+
+	var num = 81
+	sqrtNum := math.Sqrt(float64(num))
+
+	fmt.Println("SqrtNum =", sqrtNum)
+}
