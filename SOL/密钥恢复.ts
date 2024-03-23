240cc9422a084d6bbd6fd3864d0070b91a18a878
username <useremail@qq.com>
2024-03-23 14:56:47 +0800

    1

--- SOL/密钥恢复.ts
+++ SOL/密钥恢复.ts
@@ -35,3 +35,22 @@
 //     @solana/wallet-adapter-base
 // npm install --save @solana/wallet-adapter-wallets \
 //     @solana/wallet-adapter-base
+
+
+import * as web3 from '@solana/web3.js';
+// // 定义你的 Solana 私钥
+// const privateKey = Buffer.from('YOUR_PRIVATE_KEY', 'hex');
+// // 根据私钥生成公钥
+// const keypair = web3.Keypair.fromSecretKey(privateKey);
+// const publicKey = keypair.publicKey.toString();
+// console.log("Generated Public Key:", publicKey);
+
+const privateKey=Uint8Array.from([
+      174, 47, 154, 16, 202, 193, 206, 113, 199, 190, 53, 133, 169, 175, 31, 56,
+      222, 53, 138, 189, 224, 216, 117, 173, 10, 149, 53, 45, 73, 251, 237, 246,
+      15, 185, 186, 82, 177, 240, 148, 69, 241, 227, 167, 80, 141, 89, 240, 121,
+      121, 35, 172, 247, 68, 251, 226, 218, 48, 63, 176, 109, 168, 89, 238, 135,
+    ])
+const keypair = web3.Keypair.fromSeed(privateKey)
+console.log("Generated Public Key:", keypair);
+// ts-node /home/wth000/gitee/BRC20-ERC20-UI/SOL/密钥恢复.ts
