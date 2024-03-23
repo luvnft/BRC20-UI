78f69b75be32bff9d9f6e4fa1055c42604311057
username <useremail@qq.com>
2024-03-23 14:35:46 +0800

    1

---
+++ SOL/密钥恢复.ts
@@ -0,0 +1,37 @@
+// Web3.js
+// @solana/web3.js 这个库提供了很多用于与Solana交互，发送交易，从区块链上读取数据等操作的基础功能。
+
+// 可以用以下命令安装：
+
+// yarn add @solana/web3.js
+// npm install --save @solana/web3.js
+// <!-- Development (un-minified) -->
+// <script src="https://unpkg.com/@solana/web3.js@latest/lib/index.iife.js"></script>
+
+// <!-- Production (minified) -->
+// <script src="https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js"></script>
+
+
+
+
+// SPL代币（SPL-Token）
+// @solana/spl-token这个库提供了很多用于与SPL代币（SPL tokens）交互所需的JavaScript/TypeScript绑定函数。 可以用这个库来铸造新的SPL代币，进行转账，以及其他操作。
+
+// 可以用以下命令安装：
+
+// yarn add @solana/spl-token
+// npm install --save @solana/spl-token
+// <!-- Development (un-minified) -->
+// <script src="https://unpkg.com/@solana/spl-token@latest/lib/index.iife.js"></script>
+
+// <!-- Production (minified) -->
+// <script src="https://unpkg.com/@solana/spl-token@latest/lib/index.iife.min.js"></script>
+// 钱包适配器（Wallet-Adapter）
+// 这是一组用于连接Solana公链钱包的库，称为钱包适配器。 目前这些软件包支持Angular，Vue.js，以及React。钱包适配器可以帮助你的dApp很快的与诸如Phantom， Solflare以及其他一些钱包进行整合。
+
+// 可以用以下命令安装：
+
+// yarn add @solana/wallet-adapter-wallets \
+//     @solana/wallet-adapter-base
+// npm install --save @solana/wallet-adapter-wallets \
+//     @solana/wallet-adapter-base
